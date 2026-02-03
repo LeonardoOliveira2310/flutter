e6eb88b740966f32ea11e9406bd18b75a2949d22
Leonardo Oliveira <leozinho_metallica@hotmail.com>
2026-02-02 22:50:18 -0300

    Aula 1
    Variáveis e operadores básicos

--- Aulas Dart/hello.dart
+++ Aulas Dart/hello.dart
@@ -1,4 +1,12 @@
 void main() {
-  print("Hello World!");
-  print("Impressão 2");
+  // print("Hello World!");
+  // print("Impressão 2");
+  // var hello = "Hello World!";
+  // print(hello);
+
+  var num1 = 10;
+  var num2 = 2;
+  var total = num1 * num2;
+
+  print("O valor total é: $total");
 }
