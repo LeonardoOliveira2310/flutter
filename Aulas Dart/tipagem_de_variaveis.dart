1486f84054ef789b9ae3682caacf4065aea2f159
Leonardo Oliveira <leozinho_metallica@hotmail.com>
2026-02-03 22:58:24 -0300

    Sintaxe 1
    Inferência de variáveis
    Tipagem explícita de variáveis

---
+++ Aulas Dart/tipagem_de_variaveis.dart
@@ -0,0 +1,17 @@
+void main() {
+  /*Variáveis de inferencia,
+  ou seja, recebem seu tipo de acordo 
+  com o conteúdo atribuido.  
+  */
+  var texto = "Texto";
+  var numeroInteiro = 1;
+  var numeroDecimal = 1.99;
+  var verdadeiroFalso = true;
+  /*Tipagem explícita de Variáveis,
+  ou seja, váriáveis com tipos já definidos. 
+  */
+  String texto1 = "Texto";
+  int numeroInteiro1 = 1;
+  double numeroDecimal1 = 1.99;
+  bool verdadeiroFalso1 = true;
+}
