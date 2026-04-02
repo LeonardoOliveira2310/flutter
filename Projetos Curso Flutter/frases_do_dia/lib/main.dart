8dcb0c2743290687b8e7f40edde2efa0f2134264
Leonardo Oliveira <leozinho_metallica@hotmail.com>
2026-04-01 23:37:28 -0300

    Formatação de Textos
    Aprendendo sobre como utilizar as formatações de textos no Flutter

--- Projetos Curso Flutter/frases_do_dia/lib/main.dart
+++ Projetos Curso Flutter/frases_do_dia/lib/main.dart
@@ -4,13 +4,26 @@ void main() {
   runApp(
     MaterialApp(
       title: "Frases do dia",
-      home: Column(
-        children: [
-          Text('Texto 1'),
-          Text('Texto 2'),
-          Text('Texto 3'),
-          Text('Texto 4'),
-        ],
+      home: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Text(
+              'A certificação de metodologias que nos auxiliam a lidar com o desafiador cenário globalizado talvez venha a ressaltar a relatividade dos relacionamentos verticais entre as hierarquias.',
+              style: TextStyle(
+                fontSize: 35,
+                color: Colors.black,
+                fontStyle: FontStyle.normal,
+                fontWeight: FontWeight.normal,
+                letterSpacing: 2,
+                wordSpacing: 2,
+                decoration: TextDecoration.underline,
+                decorationStyle: TextDecorationStyle.solid,
+                decorationColor: Colors.greenAccent,
+              ),
+            ),
+          ],
+        ),
       ),
       // home: Row(
       //   children: [
